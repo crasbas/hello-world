72141443ef607cdb98f6d2938269340f8438c969
Edu <Edu@Edu-PC>
2017-03-02 11:43:31 +0100

    prueba 2

--- Prueba/src/Probando.java
+++ Prueba/src/Probando.java
@@ -2,4 +2,17 @@
 public class Probando {
 	
 	private String prueba;
+
+	public Probando(String prueba) {
+		super();
+		this.prueba = prueba;
+	}
+	private void sout() {
+		// TODO Auto-generated method stub
+
+	}
+	public void Prueba(){
+		System.out.println("paco");
+	}
+	
 }
